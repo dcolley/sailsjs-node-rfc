78dbe6488a1c48e7acfc03a3c8be463e1093214d
Derek Colley <derek@colley.cc>
2019-12-16 19:45:50 +0000

    Create saprfc.js

---
+++ config/saprfc.js
@@ -0,0 +1,11 @@
+
+module.exports.saprfc = {
+    S4H: {
+        user: 'derek', 
+        passwd: 'secret', 
+        ashost: '10.123.8.51', 
+        sysnr: '03', 
+        lang: 'EN',
+        client: '220', 
+    }
+}
